15295192bcb70c26cff0db81ebec0cb28a2ea878
𝕋𝕖𝕛∆𝕤 𝕡𝕣𝕠 𝕘∆𝕞𝕚𝕟𝕘 ¹⁷¹³ <tejassgupta1327@gmail.com>
2025-11-21 21:29:09 +0530

    Add run.sh script

--- run.sh
+++ run.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
-
 echo -e "WISIU Auto-Installer\n"
 
 # Check if wifi.sh exists
 if [ ! -f "wifi.sh" ]; then
-    echo "wifi.sh not found."
-    echo "Downloading fresh copy..."
+    echo "wifi.sh not found. Downloading fresh copy..."
     git clone https://github.com/tejasprogaming1713/WifiSimu-T tempfolder
-
-    # Copy wifi.sh from repo
-    if [ -f "tempfolder/wifi.sh" ]; then
-        cp tempfolder/wifi.sh .
-    else
-        echo "ERROR: wifi.sh not found inside repo."
-        rm -rf tempfolder
-        exit 1
-    fi
-
+    cp tempfolder/wifi.sh .
     rm -rf tempfolder
 fi
 
-# Final script
-FILE="wifi.sh"
-
-chmod +x "$FILE"
+chmod +x wifi.sh
 
 echo ""
 echo "=============================================="
 echo "  Auto-Setup Complete!"
 echo "  To run WISIU next time, simply type:"
 echo ""
-echo "      ./run"
+echo "      ./wifi.sh"
 echo "=============================================="
 echo ""
 
-./"$FILE"
+# Run wifi.sh
+./wifi.sh
